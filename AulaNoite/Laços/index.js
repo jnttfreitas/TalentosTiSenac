feec172682f3c8fa3d178f4ab10bf6f3e4ca3ca0
Jonathan Freitas <jntt.freitas@gmail.com>
2023-09-13 21:30:09 -0300

    aula 13/09/2023

--- AulaNoite/Laços/index.js
+++ AulaNoite/Laços/index.js
@@ -1,8 +1,58 @@
-let i = 0
+//Exercicio de Laços
 
-while (i < 10) {
+
+
+/*while (i < 10) {
 
     console.log(`O ciclista ja completou ${i} voltas.`)
 
     i++
 }
+*/
+/*
+for (let i = 0; i <= 10; i++) {
+    console.log(`O ciclista ja completou ${i} voltas.`)
+}
+*/
+/*
+const num = [14, 67, 89, 15, 23]
+
+for(let i = 0; i < 5; i++) {
+    const elemento = num[i]
+    console.log(elemento)
+}
+*/
+/*
+const num = [14, 67, 89, 15, 23]
+for (let numero of num) {
+    console.log(numero)
+}
+*/
+//Exercicio 1
+/*
+let num = 0
+
+while (num <= 10) { //Enquanto num for menor ou igual a 10 
+    if(num % 2 === 0){ // Verifica se o numero é par
+        console.log(num)
+    }
+    num++;
+}
+*/
+
+//Exercico 2
+
+let tempo = 10
+while (tempo >= 0) {
+    if(tempo === 0){
+        console.log("Booooom!!!")
+    }else if(tempo % 2 === 0) {
+        console.log("Tic")
+    }else {
+        console.log("Tac")
+    }
+
+    tempo --;
+
+}
+
